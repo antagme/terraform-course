84f9fbbd247edb65c2dab959c5701b409df10fc5
romeroaguado <romeroaguado@google.com>
2019-09-04 10:09:55 +0200

    Testing Networks

---
+++ servers.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "Firstserver" {
+ name = "thefirstserver"
+ machine_type ="n1-standard-1"
+ zone = "us-west1-a"
+
+ boot_disk {
+  initialize_params {
+   image = "debian-cloud/debian-9"
+  }
+ }
+ network_interface {
+  subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+
+  access_config {
+  }
+ }
+ metadata = {
+   foo = "bar"
+ }
+ service_account {
+ scopes = ["userinfo-email","compute-ro","storage-ro"]
+ }
+}
